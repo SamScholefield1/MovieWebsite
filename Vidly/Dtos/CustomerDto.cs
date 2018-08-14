429ad936b67d34cee2d9abacc11a2191846dd3b2
SamScholefield1 <samscholefield1@hotmail.co.uk>
2018-08-14 16:28:26 +0100

    Added membership type to datatable.

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -21,5 +21,7 @@ namespace Vidly.Dtos
         public DateTime? DateOfBirth { get; set; }
 
         public byte MembershipTypeId { get; set; }
+
+        public MembershipTypeDto MembershipType { get; set; }
     }
 }
